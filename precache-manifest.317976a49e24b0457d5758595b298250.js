1f06ab38d37a3881fdd2e2c57a0ae54014293db4
Swetha Ganapathy subramanian <swethaganapathy2@gmail.com>
2020-03-19 13:50:53 -0700

    Updates

---
+++ precache-manifest.317976a49e24b0457d5758595b298250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6666edeaf843ca1a235676943790aa",
+    "url": "/phototorecipeapp/index.html"
+  },
+  {
+    "revision": "d6438ec693083a13b8d7",
+    "url": "/phototorecipeapp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7453ee19f169c9102765",
+    "url": "/phototorecipeapp/static/css/main.7ce24492.chunk.css"
+  },
+  {
+    "revision": "d6438ec693083a13b8d7",
+    "url": "/phototorecipeapp/static/js/2.653993a6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/phototorecipeapp/static/js/2.653993a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7453ee19f169c9102765",
+    "url": "/phototorecipeapp/static/js/main.87ebae8d.chunk.js"
+  },
+  {
+    "revision": "39d6b80086a8ac2e2344",
+    "url": "/phototorecipeapp/static/js/runtime-main.c5f1dbbd.js"
+  }
+]);
